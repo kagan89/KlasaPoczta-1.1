9e448e364deeee7d1601d4b7777fc88f6eefabc0
kagan89 <mlukawski.informatyka@gmail.com>
2022-12-12 21:43:58 +0100

    Dodaj pliki projektów.

---
+++ KlasaPoczta 1.1/KlasaPoczta 1.1.cpp
@@ -0,0 +1,15 @@
+﻿#include <string.h>
+#include <iostream>
+
+int main()
+{
+    class poczta {
+
+    public:
+        std::string nadawca;
+        std::string odbiorca;
+        std::string temat;
+        std::string tresc;
+    };
+    
+}
